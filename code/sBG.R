b2d37363d5f2c87bc36588ab44b6f786ea2fe258
Sami Cheong <sami.cheong@monsanto.com>
2018-01-28 11:44:16 -0600

    HW1

--- code/sBG.R
+++ code/sBG.R
@@ -20,7 +20,7 @@ sBG.pmf = function(a, b, T = 1) {
   }
   return(ret.pmf);
 }
-
+##
 sBG.LL = function(par,churn.data,survivors,debug.flag=F) {
   pmf = sBG.pmf(exp(par[1]),exp(par[2]),length(churn.data));
   LLval = sum( log(pmf)*churn.data ) + survivors*log(1-sum(pmf));
@@ -33,15 +33,18 @@ sBG.LL = function(par,churn.data,survivors,debug.flag=F) {
 
  #alpha, beta > 0. need to impose constraint
  
- par.init=c(0,0); #concatenate
- sBG.result = optim(par=par.init,fn=sBG.LL,churn.data = sBG.insamp.inc, survivors = survivors,control=list(fnscale=-1,reltol=1e-18))
- par.final = exp(sBG.result$par)
  
- sBG.result = optim(par=sBG.result$par,fn=sBG.LL,churn.data = sBG.insamp.inc, survivors = survivors,control=list(fnscale=-1,reltol=1e-18))
- exp(sBG.result$par)
  
- print(sBG.LL(log(par.final),churn.data = sBG.insamp.inc, survivors = survivors,debug.flag=T),digits=14)
- print(sBG.LL(log(c(0.668083,3.806053)),churn.data = sBG.insamp.inc, survivors = survivors,debug.flag=T),digits=14)
- 
- pmf.12 = sBG.pmf(par.final[1],par.final[2],12)
- 1 - cumsum(pmf.12)
+ # par.init=c(0,0); #concatenate
+ # sBG.result = optim(par=par.init,fn=sBG.LL,churn.data = sBG.insamp.inc, survivors = survivors,control=list(fnscale=-1,reltol=1e-18))
+ # par.final = exp(sBG.result$par)
+ # 
+ # sBG.result = optim(par=sBG.result$par,fn=sBG.LL,churn.data = sBG.insamp.inc, survivors = survivors,control=list(fnscale=-1,reltol=1e-18))
+ # exp(sBG.result$par)
+ # 
+ # print(sBG.LL(log(par.final),churn.data = sBG.insamp.inc, survivors = survivors,debug.flag=T),digits=14)
+ # print(sBG.LL(log(c(0.668083,3.806053)),churn.data = sBG.insamp.inc, survivors = survivors,debug.flag=T),digits=14)
+ # 
+ # pmf.12 = sBG.pmf(par.final[1],par.final[2],12)	
+ # 1 - cumsum(pmf.12)
+ # 
